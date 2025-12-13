a5208419b1f2706e9fa70af008597492e05058fc
rahul3rj <jharahul3rj@gmail.com>
2025-12-13 23:23:19 +0530

    User register and login server done

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,19 @@ dotenv.config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const connectToDB = require('./db/db');
+const userRoutes = require('./routes/user.routes');
+
+connectToDB();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+app.use('/users', userRoutes);
 
 module.exports = app;
---
+++ backend/routes/user.routes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const { body } = require("express-validator");
+const userController = require('../controllers/user.controller');
+
+router.post("/register", [
+    body("email").isEmail().withMessage("Email is not valid"),
+    body("fullname.firstname").isLength({ min: 3 }).withMessage("Fullname must be at least 3 characters long"),
+    body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long"),
+], userController.registerUser);
+
+router.post('/login', [
+    body("email").isEmail().withMessage("Email is not valid"),
+    body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long"),
+], userController.loginUser);
+
+module.exports = router;
